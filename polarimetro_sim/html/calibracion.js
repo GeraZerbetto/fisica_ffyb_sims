c16852f6b8addb41c95f6804d12bca031b4534a2
Gera! <g.zerbetto@gmail.com>
2022-04-08 16:47:53 -0300

    v4
    Color and slider reset added

--- polarimetro_sim/html/calibracion.js
+++ polarimetro_sim/html/calibracion.js
@@ -1,99 +1,119 @@
-  function randn_bm2(min, max, skew) {
-    let u = 0, v = 0;
-    while(u === 0) u = Math.random() //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random()
-    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
-    
-    num = num / 10.0 + 0.5 // Translate to 0 -> 1
-    if (num > 1 || num < 0) 
-      num = randn_bm(min, max, skew) // resample between 0 and 1 if out of range
-    
-    else{
-      num = Math.pow(num, skew) // Skew
-      num *= max - min // Stretch to fill range
-      num += min // offset to min
+function randn_bm2(min, max, skew) {
+    let u = 0,
+      v = 0;
+    while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0)
+      num = randn_bm(min, max, skew); // resample between 0 and 1 if out of range
+    else {
+      num = Math.pow(num, skew); // Skew
+      num *= max - min; // Stretch to fill range
+      num += min; // offset to min
     }
-    return num
+    return num;
   }
-
-function initPola(outAn, outPol, anVal, polVal, podRot, context, canvas){
-    outAn.innerHTML = anVal.value/100;
-    outPol.innerHTML = polVal.value/100;
+  
+  function initPola(outAn, outPol, anVal, polVal, podRot, context, canvas) {
+    outAn.innerHTML = anVal.value / 100;
+    outPol.innerHTML = polVal.value / 100;
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
     alpha = calculateAlpha(anVal, polVal, podRot);
-    makeCircle(x,y,alpha.izq, Math.PI/2, 3*Math.PI/2,ctx);
-    makeCircle(x,y,alpha.der, 3*Math.PI/2, Math.PI/2,ctx);    
-}
-
-function degrees_to_radians(degrees) {
+    makeCircle(x, y, alpha.izq, Math.PI / 2, (3 * Math.PI) / 2, ctx);
+    makeCircle(x, y, alpha.der, (3 * Math.PI) / 2, Math.PI / 2, ctx);
+  }
+  
+  function degrees_to_radians(degrees) {
     var pi = Math.PI;
-    return degrees * (pi/180);
-}
-
-function calculateAlpha(analizador, polarizador, podRot){
-    analiz_deg = analizador.value/100;
-    pola_deg = polarizador.value/100;
+    return degrees * (pi / 180);
+  }
+  
+  function calculateAlpha(analizador, polarizador, podRot) {
+    analiz_deg = analizador.value / 100;
+    pola_deg = polarizador.value / 100;
     retard_deg = -pola_deg + podRot;
-
+  
     deg_net_angle = pola_deg - analiz_deg + podRot;
     deg_retard_angle = retard_deg - analiz_deg;
-
+  
     rad_angle = degrees_to_radians(deg_net_angle);
     retard_rad = degrees_to_radians(deg_retard_angle);
-    izq = Math.cos(rad_angle)**2;
-    der = Math.cos(retard_rad)**2;
+    izq = Math.cos(rad_angle) ** 2;
+    der = Math.cos(retard_rad) ** 2;
     return {
-        izq,
-        der
-    }
-}
-
-function makeCircle(x, y, alpha, start, end, canvas) {
+      izq,
+      der,
+    };
+  }
+  
+  function makeCircle(x, y, alpha, start, end, canvas) {
     canvas.beginPath();
-    string = `rgba(255,255,255,${alpha})`
+    string = `rgba(255,160,76,${alpha})`;
     canvas.arc(x, y, 200, start, end);
     canvas.fillStyle = string;
     canvas.fill();
-}
-
-refreshField = function() {
-    outputAnaliz.innerHTML = analizador.value/100;
-    outputPola.innerHTML = polarizador.value/100;
+  }
+  
+  refreshField = function () {
+    outputAnaliz.innerHTML = analizador.value / 100;
+    outputPola.innerHTML = polarizador.value / 100;
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     //alpha = calculateAlpha(analizador, polarizador, poderRotatorio)
-    alpha = calculateAlpha(analizador, polarizador, poderRotMedido)
-    makeCircle(x,y,alpha.izq, Math.PI/2, 3*Math.PI/2,ctx);
-    makeCircle(x,y,alpha.der, 3*Math.PI/2, Math.PI/2,ctx);
+    alpha = calculateAlpha(analizador, polarizador, poderRotMedido);
+    makeCircle(x, y, alpha.izq, Math.PI / 2, (3 * Math.PI) / 2, ctx);
+    makeCircle(x, y, alpha.der, (3 * Math.PI) / 2, Math.PI / 2, ctx);
+  };
+  
+  function putSample() {
+    var concentracion = document.getElementById("conc").value;
+    var podRotEsp = document.getElementById("podRotEsp").value;
+    desvEst = 3.0;
+    poderRotatorio = 2 * concentracion * podRotEsp;
+    poderRotMedido = randn_bm2(
+      poderRotatorio - desvEst,
+      poderRotatorio + desvEst,
+      1
+    );
+    document.getElementById("analizador").value = 0.0;
+    initPola(
+      outputAnaliz,
+      outputPola,
+      analizador,
+      polarizador,
+      poderRotMedido,
+      ctx,
+      canvas
+    );
+    console.log(poderRotMedido);
+    console.log(poderRotatorio);
+    
   }
-
-function putSample(){
-    var concentracion = document.getElementById("conc").value
-    var podRotEsp = document.getElementById("podRotEsp").value
-    desvEst = 3.0
-    poderRotatorio = 2 * concentracion * podRotEsp
-    poderRotMedido = randn_bm2(poderRotatorio - desvEst, poderRotatorio + desvEst, 1)
-    initPola(outputAnaliz, outputPola, analizador, polarizador, poderRotMedido, ctx, canvas);  
-    console.log(poderRotMedido)
-    console.log(poderRotatorio)
-}
-
-var analizador = document.getElementById("analizador");
-var outputAnaliz = document.getElementById("analizadorValue");
-var polarizador = document.getElementById("polarizador");
-var outputPola = document.getElementById("polarizadorValue");
-var poderRotatorio = 0.0
-//var actualizar = document.getElementById("submit");
-
-
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var x = 250;
-var y = 250;
-
-
-initPola(outputAnaliz, outputPola, analizador, polarizador, poderRotatorio, ctx, canvas);
-analizador.oninput = refreshField;
-polarizador.oninput = refreshField;
+  
+  var analizador = document.getElementById("analizador");
+  var outputAnaliz = document.getElementById("analizadorValue");
+  var polarizador = document.getElementById("polarizador");
+  var outputPola = document.getElementById("polarizadorValue");
+  var poderRotatorio = 0.0;
+  var poderRotMedido = 0.0;
+  //var actualizar = document.getElementById("submit");
+  
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var x = 250;
+  var y = 250;
+  
+  initPola(
+    outputAnaliz,
+    outputPola,
+    analizador,
+    polarizador,
+    poderRotatorio,
+    ctx,
+    canvas
+  );
+  analizador.oninput = refreshField;
+  polarizador.oninput = refreshField;
--- polarimetro_sim/html/medicion.js
+++ polarimetro_sim/html/medicion.js
@@ -1,117 +1,126 @@
 function randn_bm2(min, max, skew) {
-  let u = 0,
-    v = 0;
-  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random();
-  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    let u = 0,
+      v = 0;
+    while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
 
-  num = num / 10.0 + 0.5; // Translate to 0 -> 1
-  if (num > 1 || num < 0)
-    num = randn_bm(min, max, skew); // resample between 0 and 1 if out of range
-  else {
-    num = Math.pow(num, skew); // Skew
-    num *= max - min; // Stretch to fill range
-    num += min; // offset to min
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0)
+      num = randn_bm(
+        min,
+        max,
+        skew
+      ); // resample between 0 and 1 if out of range
+    else {
+      num = Math.pow(num, skew); // Skew
+      num *= max - min; // Stretch to fill range
+      num += min; // offset to min
+    }
+    return num;
   }
-  return num;
-}
 
-function initPola(outAn, outPol, anVal, polVal, podRot, context, canvas) {
-  outAn.innerHTML = anVal.value / 100;
-  outPol.innerHTML = polVal.value / 100;
-  context.fillStyle = "black";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  alpha = calculateAlpha(anVal, polVal, podRot);
-  makeCircle(x, y, alpha.izq, Math.PI / 2, (3 * Math.PI) / 2, ctx);
-  makeCircle(x, y, alpha.der, (3 * Math.PI) / 2, Math.PI / 2, ctx);
-}
+  function initPola(outAn, outPol, anVal, polVal, podRot, context, canvas) {
+    outAn.innerHTML = anVal.value / 100;
+    outPol.innerHTML = polVal.value / 100;
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    alpha = calculateAlpha(anVal, polVal, podRot);
+    makeCircle(x, y, alpha.izq, Math.PI / 2, (3 * Math.PI) / 2, ctx);
+    makeCircle(x, y, alpha.der, (3 * Math.PI) / 2, Math.PI / 2, ctx);
+  }
 
-function degrees_to_radians(degrees) {
-  var pi = Math.PI;
-  return degrees * (pi / 180);
-}
+  function degrees_to_radians(degrees) {
+    var pi = Math.PI;
+    return degrees * (pi / 180);
+  }
 
-function calculateAlpha(analizador, polarizador, podRot) {
-  analiz_deg = analizador.value / 100;
-  pola_deg = polarizador.value / 100;
-  retard_deg = -pola_deg + podRot;
+  function calculateAlpha(analizador, polarizador, podRot) {
+    analiz_deg = analizador.value / 100;
+    pola_deg = polarizador.value / 100;
+    retard_deg = -pola_deg + podRot;
 
-  deg_net_angle = pola_deg - analiz_deg + podRot;
-  deg_retard_angle = retard_deg - analiz_deg;
+    deg_net_angle = pola_deg - analiz_deg + podRot;
+    deg_retard_angle = retard_deg - analiz_deg;
 
-  rad_angle = degrees_to_radians(deg_net_angle);
-  retard_rad = degrees_to_radians(deg_retard_angle);
-  izq = Math.cos(rad_angle) ** 2;
-  der = Math.cos(retard_rad) ** 2;
-  return {
-    izq,
-    der,
+    rad_angle = degrees_to_radians(deg_net_angle);
+    retard_rad = degrees_to_radians(deg_retard_angle);
+    izq = Math.cos(rad_angle) ** 2;
+    der = Math.cos(retard_rad) ** 2;
+    return {
+      izq,
+      der,
+    };
+  }
+
+  function makeCircle(x, y, alpha, start, end, canvas) {
+    canvas.beginPath();
+    string = `rgba(255,160,76,${alpha})`;
+    canvas.arc(x, y, 200, start, end);
+    canvas.fillStyle = string;
+    canvas.fill();
+  }
+
+  refreshField = function () {
+    outputAnaliz.innerHTML = analizador.value / 100;
+    outputPola.innerHTML = polarizador.value / 100;
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //alpha = calculateAlpha(analizador, polarizador, poderRotatorio)
+    alpha = calculateAlpha(analizador, polarizador, poderRotMedido);
+    makeCircle(x, y, alpha.izq, Math.PI / 2, (3 * Math.PI) / 2, ctx);
+    makeCircle(x, y, alpha.der, (3 * Math.PI) / 2, Math.PI / 2, ctx);
   };
-}
 
-function makeCircle(x, y, alpha, start, end, canvas) {
-  canvas.beginPath();
-  string = `rgba(255,255,255,${alpha})`;
-  canvas.arc(x, y, 200, start, end);
-  canvas.fillStyle = string;
-  canvas.fill();
-}
+  function putSample() {
+    var concentracion = document.getElementById("conc").value;
+    //var podRotEsp = document.querySelector('input[name="muestra"]:checked').value;
+    var podRotEsp = document.getElementById("muestra").value;
+    desvEst = 3.0;
+    poderRotatorio = 2 * concentracion * podRotEsp;
+    if ((poderRotatorio > 0.0001)) {
+      poderRotMedido = randn_bm2(
+        poderRotatorio - desvEst,
+        poderRotatorio + desvEst,
+        1
+      );
+    } else {
+      poderRotMedido = 0.0;
+    }
+    document.getElementById("analizador").value = 0.0;
+    initPola(
+      outputAnaliz,
+      outputPola,
+      analizador,
+      polarizador,
+      poderRotMedido,
+      ctx,
+      canvas
+    );
+    console.log(poderRotatorio);
+    console.log(poderRotMedido);
+  }
 
-refreshField = function () {
-  outputAnaliz.innerHTML = analizador.value / 100;
-  outputPola.innerHTML = polarizador.value / 100;
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  //alpha = calculateAlpha(analizador, polarizador, poderRotatorio)
-  alpha = calculateAlpha(analizador, polarizador, poderRotMedido);
-  makeCircle(x, y, alpha.izq, Math.PI / 2, (3 * Math.PI) / 2, ctx);
-  makeCircle(x, y, alpha.der, (3 * Math.PI) / 2, Math.PI / 2, ctx);
-};
+  var analizador = document.getElementById("analizador");
+  var outputAnaliz = document.getElementById("analizadorValue");
+  var polarizador = document.getElementById("polarizador");
+  var outputPola = document.getElementById("polarizadorValue");
+  var poderRotatorio = 0.0;
+  var poderRotMedido = 0.0;
 
-function putSample() {
-  var concentracion = document.getElementById("conc").value;
-  //var podRotEsp = document.querySelector('input[name="muestra"]:checked').value;
-  var podRotEsp = document.getElementById("muestra").value;
-  desvEst = 3.0;
-  poderRotatorio = 2 * concentracion * podRotEsp;
-  poderRotMedido = randn_bm2(
-    poderRotatorio - desvEst,
-    poderRotatorio + desvEst,
-    1
-  );
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var x = 250;
+  var y = 250;
+
   initPola(
     outputAnaliz,
     outputPola,
     analizador,
     polarizador,
-    poderRotMedido,
+    poderRotatorio,
     ctx,
     canvas
   );
-  console.log(poderRotatorio);
-  console.log(poderRotMedido);
-}
-
-var analizador = document.getElementById("analizador");
-var outputAnaliz = document.getElementById("analizadorValue");
-var polarizador = document.getElementById("polarizador");
-var outputPola = document.getElementById("polarizadorValue");
-var poderRotatorio = 0.0;
-var actualizar = document.getElementById("submit");
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var x = 250;
-var y = 250;
-
-initPola(
-  outputAnaliz,
-  outputPola,
-  analizador,
-  polarizador,
-  poderRotatorio,
-  ctx,
-  canvas
-);
-analizador.oninput = refreshField;
-polarizador.oninput = refreshField;
+  analizador.oninput = refreshField;
+  polarizador.oninput = refreshField;
